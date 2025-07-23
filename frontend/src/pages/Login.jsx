758462e456f857063f2e2d5e85848aaa72c92b4c
AyaanShaikh <officialayaanshaikh@gmail.com>
2025-07-24 01:32:46 +0530

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/login", formData);
+      const response = await axios.post("https://careercompass-bs0j.onrender.com/api/login", formData);
       const { status } = response.data;
       if (status === 200) {
         const { message, userData, refreshToken, accessToken } = response.data;
