d38c7ed4161eb16cebe9670cb9d1c690456a26f1
AyaanShaikh <officialayaanshaikh@gmail.com>
2025-07-24 02:13:05 +0530

    chang

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axiosInstance.post("/api/login", formData);
+      const response = await axiosInstance.post("https://careercompass-bs0j.onrender.com/api/login", formData);
       const { status } = response.data;
       if (status === 200) {
         const { message, userData, refreshToken, accessToken } = response.data;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  server: mode === 'development' ? {
-    port: 3000,
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000, // Frontend port
     proxy: {
-      '/api': {
-        target: 'https://careercompass-bs0j.onrender.com',
+      "/api": {
+        target: "https://careercompass-bs0j.onrender.com", // Backend server URL
         changeOrigin: true,
-      }
+      },
     },
-    host: true
-  } : undefined,
+    host: true,
+  },
+  plugins: [react()],
   build: {
-    outDir: "build",
-  }
-}))
+    outDir: "build", // Specify the output directory
+  },
+});
