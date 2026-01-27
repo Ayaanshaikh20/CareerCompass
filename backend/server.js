0ef94adcf310b2aa7af23f865e3758896cddd75f
AyaanShaikh <officialayaanshaikh@gmail.com>
2026-01-27 12:06:16 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
